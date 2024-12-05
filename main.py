dc7cd26f66e80000bc1e4eee4e1f9265ea9c9c2d
koyaka <sasha.kolyaka@gmail.com>
2024-12-05 09:33:30 +0300

    Принятие фото

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import telebot;
-from googletrans import Translator;
+import telebot
+from googletrans import Translator
 from handlers.commands import Message
 
 
@@ -12,10 +12,16 @@ if __name__ == '__main__':
     @bot.message_handler(commands=['start', 'help'])
     def handle_start(message):
         message_handler.start(message)
-
     # Обработчик всех сообщений
+
     @bot.message_handler(func=lambda message: True)
     def handle_message(message):
         message_handler.translate_message(message)
+
+    @bot.message_handler(content_types=['photo'])
+    def handle_photo(message):
+        photo = message.photo[-1]
+        file_info = bot.get_file(photo.file_id)
+        bot.send_message(type(photo), type(file_info))
 
     bot.infinity_polling(interval=0, timeout=20)
